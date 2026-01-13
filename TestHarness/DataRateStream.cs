e84a00970a2e49d67c17267bdbff567fa24567ee
Ryan Esteves <snargledorf@users.noreply.github.com>
2026-01-13 17:02:57 -0500

    Revert "Remove TestHarness from solution"
    This reverts commit 454da6b8a10a8a3a1656a925beaf808455377a4f.

---
+++ TestHarness/DataRateStream.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TestHarness
+{
+    internal class DataRateStream : Stream
+    {
+        private long currentSampleBytesRead = 0;
+
+        private readonly Stopwatch stopwatch = new();
+
+        private readonly Stream stream;
+        private readonly TimeSpan sampleTimeFrame;
+
+        public DataRateStream(Stream stream, TimeSpan sampleTimeFrame)
+        {
+            this.stream = stream;
+            this.sampleTimeFrame = sampleTimeFrame;
+        }
+
+        public event EventHandler<double> DataRateUpdate;
+
+        public override bool CanRead => stream.CanRead;
+
+        public override bool CanSeek => stream.CanSeek;
+
+        public override bool CanWrite => stream.CanWrite;
+
+        public override long Length => stream.Length;
+
+        public override long Position { get => stream.Position; set => stream.Position = value; }
+
+        public override void Flush()
+        {
+            stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (!stopwatch.IsRunning)
+                stopwatch.Start();
+
+            int read = stream.Read(buffer, offset, count);
+
+            currentSampleBytesRead += read;
+
+            if (stopwatch.Elapsed >= sampleTimeFrame)
+            {
+                stopwatch.Stop();
+
+                double bytesPerSecond = currentSampleBytesRead / stopwatch.Elapsed.TotalSeconds;
+
+                OnDataRateUpdated(bytesPerSecond);
+
+                currentSampleBytesRead = 0;
+                stopwatch.Restart();
+            }
+
+            return read;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            stream.Write(buffer, offset, count);
+        }
+
+        protected virtual void OnDataRateUpdated(double e)
+        {
+            DataRateUpdate?.Invoke(this, e);
+        }
+    }
+}
---
+++ TestHarness/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+using FlexableCsvParser;
+
+namespace TestHarness
+{
+    static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Testing dataset https://www.kaggle.com/najzeko/steam-reviews-2021
+            const string filePath = @"..\..\big.csv";
+
+            using var fs = File.OpenRead(filePath);
+            using var dataRateStream = new DataRateStream(fs, TimeSpan.FromSeconds(2));
+            dataRateStream.DataRateUpdate += (_, bytesPerSecond) => {
+                double kilobytesPerSecond = bytesPerSecond / 1000;
+                double megabytesPerSecond = kilobytesPerSecond / 1000;
+
+                Console.Title = $"{megabytesPerSecond} Mb/s";
+            };
+
+            using var reader = new StreamReader(dataRateStream);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //await ReadLinesAsync(reader).ConfigureAwait(false);
+            //await Task.Factory.StartNew(() => ReadLines(reader), TaskCreationOptions.LongRunning).ConfigureAwait(false);
+            //await Task.Factory.StartNew(() => Tokenize(reader), TaskCreationOptions.LongRunning).ConfigureAwait(false);
+            await Task.Factory.StartNew(() => Parse(reader), TaskCreationOptions.LongRunning).ConfigureAwait(false);
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Read file in {stopwatch.Elapsed}");
+        }
+
+        private static async Task ReadLinesAsync(StreamReader reader)
+        {
+            while (await reader.ReadLineAsync().ConfigureAwait(false) != null) ;
+        }
+
+        private static void ReadLines(StreamReader reader)
+        {
+            while (reader.ReadLine() != null) ;
+        }
+
+        private static void Tokenize(StreamReader reader)
+        {
+            var tokenizer = new RFC4180Tokenizer();
+
+            while (tokenizer.NextToken(reader).Type != TokenType.EndOfReader)
+            {
+            }
+        }
+
+        private static void Parse(StreamReader reader)
+        {
+            var parser = new CsvParser(reader);
+
+            while (parser.TryReadRecord(out string[] _))
+            {
+            }
+        }
+    }
+}
