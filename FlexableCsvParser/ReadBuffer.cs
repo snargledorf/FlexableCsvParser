ff3d183194b343963ffec5bd22cce0d03ff41de3
Ryan Esteves <snargledorf@users.noreply.github.com>
2026-01-13 21:30:29 -0500

    Removed ReadAsync; Updated ReadBuffer to track start index and only shift buffer contents when reading new data

--- FlexableCsvParser/ReadBuffer.cs
+++ FlexableCsvParser/ReadBuffer.cs
@@ -10,45 +10,21 @@ namespace FlexableCsvParser;
 internal struct ReadBuffer(int initialBufferSize) : IDisposable
 {
     private char[] _buffer = ArrayPool<char>.Shared.Rent(initialBufferSize);
-    
+
+    private int _index;
     private int _length;
     private bool _endOfReader;
 
     public readonly bool EndOfReader => _endOfReader;
 
-    public readonly ReadOnlySpan<char> Chars => _buffer.AsSpan(0, _length);
+    public readonly ReadOnlySpan<char> Chars => _buffer.AsSpan(_index, _length);
 
     public int Length => _length;
 
-    public readonly async ValueTask<ReadBuffer> ReadAsync(TextReader reader, CancellationToken cancellationToken = default)
-    {
-        ReadBuffer readBuffer = this;
-        if (readBuffer._length == readBuffer._buffer.Length)
-            readBuffer.ExpandBuffer();
-
-        do
-        {
-            int charsRead = await reader.ReadAsync(readBuffer._buffer.AsMemory(readBuffer._length), cancellationToken)
-                .ConfigureAwait(false);
-            
-            if (charsRead == 0)
-            {
-                readBuffer._endOfReader = true;
-                break;
-            }
-            
-            readBuffer._length += charsRead;
-            
-        } while (readBuffer._length < readBuffer._buffer.Length);
-
-        return readBuffer;
-    }
-
     public void Read(TextReader reader)
     {
-        if (_length == _buffer.Length)
-            ExpandBuffer();
-        
+        CheckBuffer();
+
         do
         {
             int charsRead = reader.Read(_buffer.AsSpan(_length));
@@ -63,26 +39,34 @@ internal struct ReadBuffer(int initialBufferSize) : IDisposable
         } while (_length < _buffer.Length);
     }
 
-    private void ExpandBuffer()
-    {
-        char[] oldBuffer = _buffer;
-                
-        int newMinBufferLength = _buffer.Length < (int.MaxValue / 2) ? _buffer.Length * 2 : int.MaxValue;
-        char[] newBuffer = ArrayPool<char>.Shared.Rent(newMinBufferLength);
-                
-        oldBuffer.AsSpan(0, _length).CopyTo(newBuffer.AsSpan(0, _length));
-                
-        _buffer = newBuffer;
-                
-        ArrayPool<char>.Shared.Return(oldBuffer);
-    }
-
     public void AdvanceBuffer(int charsConsumed)
     {
         Debug.Assert(charsConsumed <= _length);
         
         _length -= charsConsumed;
-        _buffer.AsSpan(charsConsumed, _length).CopyTo(_buffer.AsSpan(0, _length));
+        _index += charsConsumed;
+    }
+
+    private void CheckBuffer()
+    {
+        if (_length == _buffer.Length)
+        {
+            char[] oldBuffer = _buffer;
+                
+            int newMinBufferLength = _buffer.Length < (int.MaxValue / 2) ? _buffer.Length * 2 : int.MaxValue;
+            char[] newBuffer = ArrayPool<char>.Shared.Rent(newMinBufferLength);
+                
+            oldBuffer.AsSpan(0, _length).CopyTo(newBuffer);
+                
+            _buffer = newBuffer;
+                
+            ArrayPool<char>.Shared.Return(oldBuffer);
+        }
+        else if (_index > 0)
+        {
+            _buffer.AsSpan(_index, _length).CopyTo(_buffer);
+            _index = 0;
+        }
     }
 
     public void Dispose()
